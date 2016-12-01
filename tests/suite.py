be91d9949e5cacd93029537ed983a49dfe410bd0
Hicham Amrati <hicham.amrati@clustervision.com>
2016-12-01 12:45:32 +0100

    Add tests for the bmcsetup module

--- tests/suite.py
+++ tests/suite.py
@@ -1,6 +1,7 @@
 import unittest
 import test_cluster as cluster
 import test_network as network
+import test_bmcsetup as bmcsetup
 import test_utils_ip as utils_ip
 import test_utils_freelist as utils_freelist
 
@@ -11,6 +12,7 @@ suite.addTests(loader.loadTestsFromModule(utils_ip))
 suite.addTests(loader.loadTestsFromModule(utils_freelist))
 suite.addTests(loader.loadTestsFromModule(cluster))
 suite.addTests(loader.loadTestsFromModule(network))
+suite.addTests(loader.loadTestsFromModule(bmcsetup))
 
 if __name__ == '__main__':
     runner = unittest.TextTestRunner(verbosity=3)
